4b3831f456a0a2e524928661b64e54efe665a6d5
José <946393361bill@gmail.com>
2026-03-26 23:54:25 +0000

    header done

--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -4,13 +4,25 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignContent: "center",
-    alignItems: "center",
+    paddingTop: 62,
   },
 
   title: {
     color: colors.green[900],
     fontSize: 12,
+  },
+
+  header: {
+    paddingHorizontal: 24,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+
+  logo: {
+    height: 32,
+    width: 38,
   },
 });
